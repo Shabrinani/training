57936956f645091f5bdc17899b147b61e0284afd
Shabrinani <nurihsanishabrina1991@gmail.com>
2026-07-15 21:10:49 +0700

    Initial commit

---
+++ challenges/code_practice_2/script.js
@@ -0,0 +1,44 @@
+// console.log("FISH FISH")
+// console.log("<><   <><")
+// console.log("   <><   ")
+// console.log("<><   <><")
+
+// console.log("MEAW MEAW")
+// console.log(" /\\_/\\ ")
+// console.log("( o.o )")
+// console.log(" > ^ < ")
+
+// // Declaring variables
+// var firstName = "Shabrina"
+// var lastName = "Ihsani"
+// var favoriteNumber = 17
+
+// // Displaying the values
+// console.log("First Name: " + firstName)
+// console.log("Last Name: " + lastName)
+// console.log("Favorite Number: " + favoriteNumber)
+
+// Grade Checker
+var score = prompt("Enter your score: ");
+
+if (score > 90) {
+    console.log("Grade A")
+} else if (score > 75) {
+    console.log("Grade B")
+} else if (score > 60) {
+    console.log("Grade C")
+} else {
+    console.log("Grade D")
+}
+
+// // Member Discount Calculator
+// var totalPayment = 200000
+// var member = true
+
+// if (member) {
+//     totalPayment = totalPayment * 0.9
+//     console.log("Total payment for member: " + totalPayment)
+// } else {
+//     console.log("Total payment for non-member: " + totalPayment)
+// }
+
