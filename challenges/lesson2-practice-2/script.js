d81a5d3ca0a1cf78b3cbbba96bf975c2afca1225
Shabrinani <nurihsanishabrina1991@gmail.com>
2026-07-15 21:43:27 +0700

    lesson 2 practice 2

---
+++ challenges/lesson2-practice-2/script.js
@@ -0,0 +1,16 @@
+// Task 1
+var movieTitle = "Captain Marvel"
+var rating = 6.8
+var yearOfIssue = 2019
+var isForAllAges = false
+
+// Task 2
+var bookTitle = "The Quantum Universe"
+var author = ["Brian Cox", "Jeff Forshaw"]
+var yearOfPublication = 2011
+var isScience = true
+
+console.log("Book title: " + bookTitle)
+console.log("Author: " + author[0] + " and " + author[1])
+console.log("Year of Publication: " + yearOfPublication)
+console.log("Is the book categorized as science: " + isScience)
