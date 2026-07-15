aa3eb9ea6fa5b220d15e8071d701d6e00179254f
Shabrinani <nurihsanishabrina1991@gmail.com>
2026-07-15 22:20:42 +0700

    lesson 4 practice 1

---
+++ challenges/lesson4-practice-1/script.js
@@ -0,0 +1,16 @@
+// Task 1
+var name = "Shabrina"
+
+for (var i = 0; i < 20; i++) {
+    console.log(name)
+}
+
+// Task 2
+for (var i = 1; i <= 20; i++) {
+    console.log(i)
+}
+
+// Task 3
+for (var i = 1 ; i <= 20; i++) {
+    console.log(i*5)
+}
