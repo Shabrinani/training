e2411d849348ca36ff9619bf4502eedfe7041709
Shabrinani <nurihsanishabrina1991@gmail.com>
2026-07-15 21:31:32 +0700

    add lesson 1 code practice 2

---
+++ challenges/lesson2-practice-2/script.js
@@ -0,0 +1,18 @@
+console.log("    *    ")
+console.log("   ***   ")
+console.log("  *****  ")
+console.log(" ******* ")
+console.log("   |||   ")
+console.log("\n")
+
+console.log("FISH FISH")
+console.log("<><   <><")
+console.log("   <><   ")
+console.log("<><   <><")
+console.log("\n")
+
+console.log("MEAW MEAW")
+console.log(" /\\_/\\ ")
+console.log("( o.o )")
+console.log(" > ^ < ")
+
